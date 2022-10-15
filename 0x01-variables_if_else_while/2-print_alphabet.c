c5113e816070eaff8ddc678dc1c97f329abd75b5
Pmoyse-techy <promisemoyse10@gmail.com>
2022-10-15 00:57:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,15 @@
 /**
  *main-program entry point.
  *Return:0 if no error, non zero if error.
- */
+ **/
 int main(void)
 {
 	char i;
-        for (i = 'a'; i <= 'z'; i++)
-{
-        putchar(i);
-}
-       putchar('\n');
-      return (0);
-}
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

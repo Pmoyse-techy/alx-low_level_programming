3c9d21c666972136b69a83bc57477d6c31ad8821
Pmoyse-techy <promisemoyse10@gmail.com>
2022-10-18 10:12:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <main.h>
+#include "main.h"
 
-\**
-*main
-*\
-	int main(void)
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	_purchar("start of the outer loop\n");
+		int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	       int count, sz;
 
-	return(0);
+		sz = sizeof(str) / sizeof(int);
+		for (count = 0; count < sz; count++)
+{
+		_putchar(str[count]);
 }
+		_putchar('\n');
+		return (0);
+}
+

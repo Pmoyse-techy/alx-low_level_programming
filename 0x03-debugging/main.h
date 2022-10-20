5dbe15fe582863427a7e082b4b1dc5d461d7c242
Pmoyse-techy <promisemoyse10@gmail.com>
2022-10-20 14:21:06 -0700

    0 task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include  <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print _remaining _days(int month, int day, int year);
+int convert_day(int month, int day);
+void print _times_table (int n)
+
+
+#endif
+

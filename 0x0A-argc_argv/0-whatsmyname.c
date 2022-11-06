5e037cb0d236dd467597093eed8988c74cc72eef
Pmoyse-techy <promisemoyse10@gmail.com>
2022-11-06 01:50:05 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,12 @@
  * @argc:int
  * @argv:string
  * Return:int
- * /
+ */
 int main(int argc, char *argv[])
 {
-		if (argc == 1)
-{
-    printf("%s\n", argv[0]);
-								}
-			return (0);
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
 }
